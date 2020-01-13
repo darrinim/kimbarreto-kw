7e1099d804ac9f14dda92f8ce91712d5e537a868
darrinim <darrinj.im@gmail.com>
2020-01-13 16:40:25 -0500

    added Contact/Footer component, added styling most of mobile responsiveness

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,21 +5,35 @@ const Contact = () => {
 
   return(
     <div className="contactContainer">
-      <div className="contactInfoWrap">
-        <h2>Contact</h2>
-        <ul>
-          <li>Address</li>
-          <li>Number</li>
-          <li>Email</li>
-        </ul>
-      </div>
-      <div className="contactFormWrap">
-        <form>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-        </form>
+      <div className="contactOuterWrap">
+        <div className="contactInfoWrap">
+          <h2>Contact</h2>
+            <div>
+              <span><i class="fas fa-map-marker-alt icon"></i></span>
+              <span>
+                <a
+                  target="blank"
+                  href="https://goo.gl/maps/RUF9zQzJUwbAEYvK6">2200 Fletcher Ave, Fort Lee, NJ 07024
+                </a>
+              </span>
+            </div>
+            <div>
+              <span><i class="fas fa-phone-alt icon"></i></span>
+              <span><a target="blank" href="tel:2015938900">Office: (201) 592-8900</a></span>
+            </div>
+            <div>
+              <span><i class="fas fa-envelope icon"></i></span>
+              <span><a target="blank" href="mailto:info@kimberlybarreto.com">info@kimberlybarreto.com</a></span>
+            </div>
+        </div>
+        <div className="contactFormWrap">
+          <form>
+            <input type="text" placeholder="Name"></input>
+            <input type="text" placeholder="Email"></input>
+            <input type="text" placeholder="What's on your mind?"></input>
+            <input type="submit" value="Send"></input>
+          </form>
+        </div>
       </div>
     </div>
   )
