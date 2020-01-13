184528a5ec30cf2f53c3ea7769197a93f61bee24
darrinim <darrinj.im@gmail.com>
2020-01-13 13:55:56 -0500

    added About component/styled, need to resize for actual content

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import Header from './components/header/Header';
 import LandingHero from './components/LandingHero/LandingHero';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <LandingHero />
+      <About />
     </div>
   );
 }
