ff80b306d6e192f1433816de8c7ee387970cdb5f
darrinim <darrinj.im@gmail.com>
2020-01-13 12:42:22 -0500

    added/styled landingHero component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Header from './components/header/Header';
+import LandingHero from './components/LandingHero/LandingHero';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <LandingHero />
     </div>
   );
 }
---
+++ src/components/LandingHero/LandingHero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingHero.css'
+
+const LandingHero = () => {
+
+  return(
+    <div className="landingHeroContainer">
+      <img className="landingHeroImage" src="https://res.cloudinary.com/darrin-im/image/upload/v1578936272/real-estate-landing_copy_ypt9qg.jpg" />
+    </div>
+  )
+}
+
+export default LandingHero;
