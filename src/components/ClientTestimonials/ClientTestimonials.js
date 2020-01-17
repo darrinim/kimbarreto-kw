26c58dfefcf288a3ce6861e813e9dd4a51ebe3ff
darrinim <darrinj.im@gmail.com>
2020-01-16 22:09:46 -0500

    was able to extract reviews

--- src/components/ClientTestimonials/ClientTestimonials.js
+++ src/components/ClientTestimonials/ClientTestimonials.js
@@ -4,22 +4,48 @@ import axios from 'axios';
 
 const ClientTestimonials = () => {
 
-
   const [ reviews, setReview ] = useState([]);
 
   // useEffect(() => {
   //   fetch('http://www.zillow.com/webservice/ProReviews.htm?zws-id=X1-ZWz17fzq7msjrf_7j3qs&output=json&screenname=ROBfortheJOB&count=10')
-  //   .then(reviews => setReview(reviews))
+  //   .then(response => response.json())
+  //   .then(reviews => {
+  //     setReview(reviews.response.results.proReviews.review)
+  //     console.log('inside useEffect', reviews.response.results.proReviews.review)})
   // }, [])
 
-  const getReviews = async () => {
-    const response = await axios.get('http://www.zillow.com/webservice/ProReviews.htm?zws-id=X1-ZWz17fzq7msjrf_7j3qs&output=json&screenname=ROBfortheJOB&count=10')
-    .then((response) => {
-      let review = response.data.response.results.proReviews.review;
-      console.log('in getReviews', review);
-      setReview(review)
-  });
-}
+  // empty array reps the dependencies for useEffect hook
+  // if something in that array changes, that function will run again
+    // (always will be array)
+    // empty array indicates that it only needs to run once (like componentdidmount)
+    // look into componentdidUpdate
+
+    // useEffect(async () => {
+    //   const response = await fetch('http://www.zillow.com/webservice/ProReviews.htm?zws-id=X1-ZWz17fzq7msjrf_7j3qs&output=json&screenname=ROBfortheJOB&count=10')
+    //   const reviews = await response.json()
+    //     setReview(reviews.response.results.proReviews.review)
+    //     console.log('inside useEffect', reviews.response.results.proReviews.review)
+    // }, [])
+    //the above gives warning, look at it
+
+    useEffect(() => {
+      async function getReviews () {
+        const response = await fetch('http://www.zillow.com/webservice/ProReviews.htm?zws-id=X1-ZWz17fzq7msjrf_7j3qs&output=json&screenname=ROBfortheJOB&count=10')
+        const reviews = await response.json()
+        setReview(reviews.response.results.proReviews.review)
+        console.log('inside useEffect', reviews.response.results.proReviews.review)
+      }
+      getReviews();
+    }, [])
+
+//   const getReviews = async () => {
+//     const response = await axios.get('http://www.zillow.com/webservice/ProReviews.htm?zws-id=X1-ZWz17fzq7msjrf_7j3qs&output=json&screenname=ROBfortheJOB&count=10')
+//     .then((response) => {
+//       let review = response.data.response.results.proReviews.review;
+//       console.log('in getReviews', review);
+//       setReview(review)
+//   });
+// }
 // getReviews();
 
 //need && somewhere, but where
@@ -29,7 +55,11 @@ const ClientTestimonials = () => {
 
   return(
     <div>
-      {console.log('inside return', reviews)}
+      {reviews.map(review => {
+        return(
+          <p>{review.description}</p>
+        )
+      })}
     </div>
   )
 }
